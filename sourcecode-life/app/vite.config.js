a2e2de00e982c4918857c1260c7d6405d57ba5cb
Kytholek <103764775+Kytholek@users.noreply.github.com>
2026-04-20 09:28:02 -0600

    sd

--- sourcecode-life/app/vite.config.js
+++ sourcecode-life/app/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sourcecode-life/app/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
